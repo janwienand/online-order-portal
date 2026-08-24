72484ceb8caabd9c6dddd03b61526ebd5c6ea4cb
Fortify Remediation Aviator <janwienand@gmail.com>
2026-08-24 20:14:39 +0200

    fix(xml-external-entity-injection): XML External Entity Injection in UserController.java:572
    Generated by OpenText Core SAST Aviator.
    FoD release 337, scan 1283, issue 79953.

--- src/main/java/com/microfocus/example/web/controllers/UserController.java
+++ src/main/java/com/microfocus/example/web/controllers/UserController.java
@@ -520,7 +520,13 @@ public class UserController extends AbstractBaseController {
         String xmlContent = "";
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try {
-			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, false);
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			dbf.setXIncludeAware(false);
+			dbf.setExpandEntityReferences(false);
 	        DocumentBuilder db = dbf.newDocumentBuilder();
 	        Document doc = db.parse(fpath.toFile());
 	        try (ByteArrayOutputStream bytesOutStream = new ByteArrayOutputStream()) {
@@ -582,7 +588,13 @@ public class UserController extends AbstractBaseController {
         
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try {
-			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, false);
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			dbf.setXIncludeAware(false);
+			dbf.setExpandEntityReferences(false);
 	        DocumentBuilder db = dbf.newDocumentBuilder();
 	        Document doc = db.parse(new InputSource(new StringReader(newXMLContent)));
 	        Path temp = Files.createTempFile("iwa", ".xml");
